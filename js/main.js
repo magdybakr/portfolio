1fef64cb5ed4b0ef1dc32d31120eadfa8aaa19c9
magdy <37843193+magdybakr@users.noreply.github.com>
2020-07-09 10:19:43 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -19,3 +19,16 @@ $(window).scroll(function() {
     $("#mainNav").css("background", "rgba(0, 0, 0, 0.75)");
   }
 });
+
+$(function(){
+    // this will get the full URL at the address bar
+    var url = window.location.href; 
+
+    // passes on every "a" tag 
+    $("#mainNav a").each(function() {
+            // checks if its the same on the address bar
+        if(url == (this.href)) { 
+            $(this).closest("li").addClass("active");
+        }
+    });
+});
