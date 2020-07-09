f3440d6683b79d0fbce2f8ac55405a65712dc297
magdy <37843193+magdybakr@users.noreply.github.com>
2020-07-09 10:29:12 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ $(function(){
     var url = window.location.href; 
 
     // passes on every "a" tag 
-    $("#mainNav a").each(function() {
+    $("#mainNav a").click.each(function() {
             // checks if its the same on the address bar
         if(url == (this.href)) { 
             $(this).closest("li").addClass("live-section");
